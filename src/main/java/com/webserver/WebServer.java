df0bd1d1abb46791518d8173144578b57c492ec1
Jitesh Golatkar <jbgolatkar@gmail.com>
2024-11-21 22:00:39 -0800

    adding file reading to serve html page

--- src/main/java/com/webserver/WebServer.java
+++ src/main/java/com/webserver/WebServer.java
@@ -5,6 +5,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class WebServer {
+    private static final String ROOT = "/";
+    private static final String WEB_DIRECTORY = "F:\\Coding-Challenges-Web-Server\\src\\main\\www";
     private final int port;
     private static final String DEFAULT_PATH = "/";
 
@@ -34,15 +36,29 @@ public class WebServer {
                 System.out.println("Request: " + requestLine);
 
                 final String[] requestData = requestLine.split(" ");
-                final String requestPath = getRequestPath(requestData);
+                String requestPath = ROOT.equals(getRequestPath(requestData)) ? "/index.html" : getRequestPath(requestData);
 
                 // Write HTTP response
                 final BufferedWriter serverResponseWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                final File file = new File(WEB_DIRECTORY + requestPath);
 
-                serverResponseWriter.write("HTTP/1.1 200 OK\r\n");
-                serverResponseWriter.write("Content-Type: text/plain\r\n");
-                serverResponseWriter.write("\r\n");  // End of headers
-                serverResponseWriter.write(String.format("Requested path: %s\r\n", requestPath));
+                if(file.exists() && file.isFile()) {
+
+                    serverResponseWriter.write("HTTP/1.1 200 OK\r\n");
+                    serverResponseWriter.write("\r\n");  // End of headers
+
+                    try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            serverResponseWriter.write(line);
+                            serverResponseWriter.newLine();
+                        }
+                    }
+                } else {
+                    serverResponseWriter.write("HTTP/1.1 404 Not Found\r\n");
+                    serverResponseWriter.write("\r\n");  // End of headers
+                }
+
                 serverResponseWriter.flush();
 
                 // Close connection
